00cb95ecf81de95f0ff0c8e4930a1b338fd803e3
Christian Cuesta <christianluminor@gmail.com>
2023-10-30 12:26:12 +0100

    Initial commit

---
+++ src/test/java/com/example/form_backend_java/FormBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.form_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
