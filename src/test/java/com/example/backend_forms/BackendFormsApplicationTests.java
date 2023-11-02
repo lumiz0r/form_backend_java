825b896a617071422be9d575a90ba9dab1ed3d84
Christian Cuesta <christianluminor@gmail.com>
2023-11-02 10:32:59 +0100

    New project

---
+++ src/test/java/com/example/backend_forms/BackendFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
