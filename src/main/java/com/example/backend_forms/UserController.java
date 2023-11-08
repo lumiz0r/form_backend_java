d6abecfd0c91f545953a6414385104614aa4b15c
Christian Cuesta <christianluminor@gmail.com>
2023-11-08 13:40:46 +0100

    POST Works properly

--- src/main/java/com/example/backend_forms/UserController.java
+++ src/main/java/com/example/backend_forms/UserController.java
@@ -3,13 +3,15 @@ package com.example.backend_forms;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
@@ -26,7 +28,14 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<User>> getUserById(@PathVariable("id") ObjectId id) {
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable("id") String id) {
         return new ResponseEntity<Optional<User>>(userService.getUserById(id), HttpStatus.OK);
     }
+
+    @PostMapping()  
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+    
 }
--- src/main/java/com/example/backend_forms/UserService.java
+++ src/main/java/com/example/backend_forms/UserService.java
@@ -3,7 +3,6 @@ package com.example.backend_forms;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,11 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Optional<User> getUserById(ObjectId id) {
+    public Optional<User> getUserById(String id) {
         return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.insert(user);
     }
 }
